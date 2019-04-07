22849f8863e9bcf4a9e5dcdab77dbb50d38a57b7
furkantarikgocmen <f.tarikgocmen@outlook.com>
2019-04-07 14:32:17 +0300

    I Learn Node.js

--- node-ogreniyorum/webSocket/room/app.js
+++ node-ogreniyorum/webSocket/room/app.js
@@ -6,13 +6,34 @@ const server = http.createServer((req,res) => {
 server.listen(3000);
 
 const io = socketio.listen(server);
-io.on('connection', (socket) =>{
+io.on('connection', (socket) => {
+
     socket.on('joinRoom', (data) => {
-       socket.join(data.name, () => {
-           let count  = io.sockets.adapter.rooms[data.name].length;
-           //socket.to(data.name).emit('new join',{count}); //Kullanıcı Dışında bütün odadakilere
-           io.to(data.name).emit('new join',{count}); // Kullanıcı Dahil bütün odadakilere
-           socket.emit('log',{message : 'odaya girdiniz'});
-       });
+        socket.join(data.name, () => {
+            //socket.to(data.name).emit('new join',{count}); //Kullanıcı Dışında bütün odadakilere
+            io.to(data.name).emit('new join', {count: getOnlineCount(io, data)}); // Kullanıcı Dahil bütün odadakilere
+            socket.emit('log', {message: 'odaya girdiniz'});
+            console.log(getOnlineCount(io, data));
+
+            getRoom(socket);
+        });
+    });
+
+    socket.on('leaveRoom', (data) => {
+        socket.leave(data.name, () => {
+            io.to(data.name).emit('LeavedRoom', {count: getOnlineCount(io, data)});
+            console.log(getOnlineCount(io, data));
+
+            getRoom(socket);
+        });
     });
 });
+const getOnlineCount = (io,data) =>  {
+    const room = io.sockets.adapter.rooms[data.name];
+    return room ? room.length : 0;
+};
+
+const getRoom = (socket) => {
+    const rooms = Object.keys(socket.rooms);
+    console.log(rooms)
+};
